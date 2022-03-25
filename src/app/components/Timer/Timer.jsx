d2361043b235a8d7a913fa00b49005065bea4517
Henish Patel <200120107182@git.org.in>
2022-03-25 11:15:50 +0530

    Responsive bug Fixes.

--- src/app/components/Timer/Timer.jsx
+++ src/app/components/Timer/Timer.jsx
@@ -5,52 +5,20 @@ import "./timer.css";
 function Counter() {
   return (
     <div className="timer">
-      <h1>Time Remaining</h1>
+      <h2>Time Remaining</h2>
       <div className="counter">
         <FlipCountdown
+          className="timer-item"
           titlePosition="bottom"
+          size={"big"}
           theme="dark"
-          hideYear
           hideMonth
-          hideDay
-          hideMinute
-          hideSecond
+          hideYear
           dayTitle="Days"
           hourTitle="Hours"
           minuteTitle="Minutes"
           secondTitle="Seconds"
-          endAt={"2022-03-26 01:26:58"}
-        />
-        <h1>:</h1>
-        <FlipCountdown
-          titlePosition="bottom"
-          theme="dark"
-          hideYear
-          hideMonth
-          hideHour
-          hideDay
-          hideSecond
-          dayTitle="Days"
-          hourTitle="Hours"
-          minuteTitle="Minutes"
-          secondTitle="Seconds"
-          endAt={"2022-03-26 01:26:58"}
-        />
-        <h1>:</h1>
-        <FlipCountdown
-          titlePosition="bottom"
-          theme="dark"
-          hideYear
-          hideMonthhideYear
-          hideMonth
-          hideMinute
-          hideHour
-          hideDay
-          dayTitle="Days"
-          hourTitle="Hours"
-          minuteTitle="Minutes"
-          secondTitle="Seconds"
-          endAt={"2022-03-26 01:26:58"}
+          endAt={"2022-03-29 11:00:00"}
         />
       </div>
     </div>
