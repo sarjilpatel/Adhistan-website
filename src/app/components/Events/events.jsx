4360dc691f353aff3ff8cf293b1954f49eb7e1a6
Henish Patel <200120107182@git.org.in>
2022-03-25 22:38:31 +0530

    Events Section added.

--- src/app/components/Events/events.jsx
+++ src/app/components/Events/events.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import "./events.css";
 
 export const EventsContainer = ({ children }) => (
-  <div className="events">{children}</div>
+  <div className="events">
+    <h2 className="events-header">Events</h2>
+    <div className="event-wrap">{children}</div>
+  </div>
 );
 
-export const EventSection = ({ title, image }) => (
-  <div className="events-section">
-    <img src={image} alt={title} />
-    <h2>{title}</h2>
-    {/* {events.map((item) => (
-      <span>
-        {item} <br />
-      </span>
-    ))} */}
+export const EventSection = ({ title, events, image }) => (
+  <div className="event-section">
+    <div className="event-section-wrapper">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <div className="events-wrapper">
+        {events.map((item) => (
+          <span>
+            {item.title} <br />
+          </span>
+        ))}
+      </div>
+    </div>
   </div>
 );
--- src/app/components/Events/index.jsx
+++ src/app/components/Events/index.jsx
@@ -1,12 +1,104 @@
 import React from "react";
 import { EventsContainer, EventSection } from "./events";
+import "./events.css";
 
 const data = [
   {
     id: 1,
     title: "Technical Events",
-    image: "../assets/events/technical.png",
+    image: require("../assets/events/technical.png"),
     registrationLink: "",
+    events: [
+      {
+        title: "DreamTech",
+      },
+      {
+        title: "Blind Coding",
+      },
+      {
+        title: "Guessozonia",
+      },
+      {
+        title: "Hardwar",
+      },
+      {
+        title: "Programmer Date",
+      },
+      {
+        title: "Web Mania",
+      },
+      {
+        title: "Photo Bomb",
+      },
+      {
+        title: "Office Mania",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Workshops",
+    image: require("../assets/events/workshop-event.png"),
+    registrationLink: "",
+    events: [
+      {
+        title: "DreamTech",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Non Tech Events",
+    image: require("../assets/events/non-tech-events.png"),
+    registrationLink: "",
+    events: [
+      {
+        title: "Trust Your Partner",
+      },
+      {
+        title: "The Escape Plan",
+      },
+      {
+        title: "Meme Guruji",
+      },
+      {
+        title: "Clash Of Time",
+      },
+      {
+        title: "I’m Holmes",
+      },
+      {
+        title: "Cricket-EPhantoms",
+      },
+      {
+        title: "Treasure Hunt",
+      },
+      {
+        title: "Gaming Events",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Fun Games",
+    image: require("../assets/events/fun-games.png"),
+    registrationLink: "",
+    events: [
+      {
+        title: "DreamTech",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Cultural Events",
+    image: require("../assets/events/cultural-events.png"),
+    registrationLink: "",
+    events: [
+      {
+        title: "DreamTech",
+      },
+    ],
   },
 ];
 
@@ -14,7 +106,12 @@ function Events() {
   return (
     <EventsContainer>
       {data.map((item) => (
-        <EventSection title={item.title} image={item.image} id={item.id} />
+        <EventSection
+          title={item.title}
+          image={item.image}
+          id={item.id}
+          events={item.events}
+        />
       ))}
     </EventsContainer>
   );
