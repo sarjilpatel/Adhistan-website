28a495a47395f58fcf377564fc04ae9230aa434f
Henish Patel <200120107182@git.org.in>
2022-03-25 14:07:30 +0530

    changes background.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { useContext } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import About from "./app/components/About/about";
 import Footer from "./app/components/Footer/footer";
 import Header from "./app/components/Header/Header";
-import Hero from "./app/components/Hero";
 import Home from "./app/components/Home/home";
-import Timer from "./app/components/Timer";
 
 function App() {
   useContext(() => {
